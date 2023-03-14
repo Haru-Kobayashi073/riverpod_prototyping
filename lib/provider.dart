cf52394032b18fffb6ad23d4f9e1b8d51c782fb4
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2023-03-15 03:31:09 +0900

    countDataProviderの定義

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_countup/data/count_data.dart';
 
 final titleProvider = Provider<String>(
   (ref) => 'RiverPod Demo',
@@ -8,3 +9,6 @@ final messageProvider =
     Provider<String>((ref) => 'You have pushed the button this many times:');
 
 final countProvider = StateProvider<int>((ref) => 0);
+
+final countDataProvider = StateProvider<CountData>(
+    (ref) => const CountData(count: 0, countUp: 0, countDown: 0));
